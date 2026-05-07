c9daa85ae7ee519f4130af5219ec976b2e4d59ee
Bishal Shahi <bishal@aigeeks.ae>
2026-05-07 15:05:14 +0545

    chore(setup): initialize nestjs backend with prisma, swagger and validation

---
+++ backend/src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('Salon Appointment Management API')
+  .setDescription(
+    'API documentation for the Salon Appointment & Time Slot Management System',
+  )
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+
+const document = SwaggerModule.createDocument(app, config);
+
+SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
